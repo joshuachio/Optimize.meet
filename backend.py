b9f7f18e8f1d1694c75a39621b21554e5b23a971
joshuachio <jchio@gatech.edu>
2022-05-09 20:03:39 -0500

    Updated new datetime organization

--- backend.py
+++ backend.py
@@ -1,64 +1,71 @@
 import time
-import numpy
-import calendar
+# import numpy
+import calendar as cal
 import datetime
 
+calendar = {}
 
+for y in range(2022, 2026):
+    yearDict = {}
+    for month in range(1, 13):
+        monthDict = {}
+        if month == 4 or month == 6 or month == 9 or month == 11:
+            monthDict = {}
+            for d in range(1, 31):
+                date = datetime.datetime(y, month, d)
+                monthDict[date] = []
+        elif month == 2:
+            if y % 4 == 0:
+                for d in range(1, 30):
+                    date = datetime.datetime(y, month, d)
+                    monthDict[date] = []
+            else:
+                for d in range(1, 29):
+                    date = datetime.datetime(y, month, d)
+                    monthDict[date] = []
+        else:
+            for d in range(1, 32):
+                date = datetime.datetime(y, month, d)
+                monthDict[date] = []
+        yearDict[month] = monthDict
+    calendar[y] = yearDict
 
-days = {
-   0:'Monday',
-   1:'Tuesday',
-   2:'Wednesday',
-   3:'Thursday',
-   4:'Friday',
-   5:'Saturday',
-   6:'Sunday'
+# days = {
+#    0:'Monday',
+#    1:'Tuesday',
+#    2:'Wednesday',
+#    3:'Thursday',
+#    4:'Friday',
+#    5:'Saturday',
+#    6:'Sunday'
+# }
+   
+repeat = {
+   1: datetime.timedelta(days=1),
+   2: datetime.timedelta(weeks=1)
 }
-   
 
-   
-   
-   
-
-
-
-class Recurring:
-
-   def __init__(self, type):
-      if type == 1:
-         self.reccurance = "DAILY"
-      elif type == 2:
-         self.reccurance = "WEEKLY"
 
 class Event:
-   
-   def __init__(self, start_time: datetime.time, end_time: datetime.time, start_date: datetime.date,
-                 end_date: datetime.date, event_name: str, recurring: Recurring, description: str):
-      self.start_time = start_time
-      self.end_time = end_time
-      self.start_date = start_date
-      self.end_date = end_date
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime,
+                event_name: str, description: str, recurring: int):
+      self.startDateTime = startDateTime
+      self.endDateTime = endDateTime
       self.event_name = event_name
-      self.recurring = recurring #needs to be a "recurring type" with weekly/daily/etc
       self.description = description
+      self.recurring = repeat[recurring]
 
-   def setStartTime(self, time):
-      self.start_time = time
+   def setStartDatetime(self, datetime):
+      self.startDateTime = datetime
 
-   def setEndTime(self, time):
-      self.end_time = time
-
-   def setStartDate(self, date):
-      self.start_date = date
-
-   def setEndDate(self, date):
-      self.end_date = date
+   def setEndDatetime(self, datetime):
+      self.endDateTime = datetime
 
    def setEventName(self, name):
       self.event_name = name
 
-   def setRecurring(self, recurring: Recurring):
-      self.recurring = recurring
+   def setRecurring(self, recurring):
+      self.recurring = repeat[recurring]
 
    def setDescription(self, description):
       self.description = description
@@ -68,75 +75,80 @@ class Event:
 
 class Course(Event):
 
-   def __init__(self, start_time: datetime.time, end_time: datetime.time, start_date: datetime.date,
-                 end_date: datetime.date, event_name: str, recurring: Recurring, description: str):
-      super().__init__(start_time, end_time, start_date, end_date, event_name, recurring, description)
-      self.
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, description: str, recurring: int):
+       super().__init__(startDateTime, endDateTime, event_name, description, recurring)
 
 class Meeting(Event):
 
-   def __init__(self, start_time: datetime.time, end_time: datetime.time, start_date: datetime.date,
-                 end_date: datetime.date, event_name: str, recurring: Recurring, description: str):
-      super().__init__(start_time, end_time, start_date, end_date, event_name, recurring, description)
-      self.
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, description: str, recurring: int):
+       super().__init__(startDateTime, endDateTime, event_name, description, recurring)
 
 
 class Personal(Event):
 
-   def __init__(self, start_time: datetime.time, end_time: datetime.time, start_date: datetime.date,
-               end_date: datetime.date, event_name: str, recurring: Recurring, description: str):
-      super().__init__(start_time, end_time, start_date, end_date, event_name, recurring, description)
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, description: str, recurring: int):
+      super().__init__(startDateTime, endDateTime, event_name, description, recurring)
 
 
-class Day:
+# class Day:
    
-   def __init__(self, day: datetime.date, dayofweek):
-       self.day = day
-       self.dayofweek = days[dayofweek]
-       self.eventList = {}
+#    def __init__(self, day: datetime.date, dayofweek):
+#        self.day = day
+#        self.dayofweek = days[dayofweek]
+#        self.eventList = {}
    
-   def dayofweek(self):
-      return self.dayofweek
+#    def dayofweek(self):
+#       return self.dayofweek
    
-   def addToEventList(self, addEvent: Event):
-      if (addEvent.start_date is self.day):
-         self.eventList[addEvent.start_time] = (addEvent)
+#    def addToEventList(self, addEvent: Event):
+#       if (addEvent.start_date is self.day):
+#          self.eventList[addEvent.start_time] = (addEvent)
 
-   def displayEvents(self):
-      keyOrder = list(self.eventList.keys())
-      keyOrder.sort()
-      for time in keyOrder:
-         self.display(time, self.eventList[time].event_name)
+#    def displayEvents(self):
+#       keyOrder = list(self.eventList.keys())
+#       keyOrder.sort()
+#       for time in keyOrder:
+#          self.display(time, self.eventList[time].event_name)
    
-   def display(self, time: datetime.time, eventName):
-      ##some frontend for displaying stuff
-      None
+#    def display(self, time: datetime.time, eventName):
+#       ##some frontend for displaying stuff
+#       None
 
 
-class Month:
-   def __init__(self, month: datetime.month):
-      self.month = month #an integer (1-12) representing the month
-      self.daysList = {}
+# class Month:
+#    def __init__(self, month, year):     
+#       self.month = month #an integer (1-12) representing the month
+#       self.daysList = [None]
+#       if month == 4 or month == 6 or month == 9 or month == 11:
+#          pass
+#       elif month == 28 or month == 29:
+#          pass
+#       else:
+#          for i in range(1, 32):
+#             tempDate = datetime.date(year, month, i)
+#             temp = Day(tempDate, )
+#             self.daysList.append(temp)
 
-   def addDays(self, day: Day):
-      self.daysList[day.day.day] = day #just maps the day (1-31) to that Day object
 
-   def display(self):
-      #display the month visually
-      pass
 
-class Year:
-   def __init__(self, year: datetime.year):
-      self.year = year
-      self.monthsList = {}
+#    def addDays(self, day: Day):
+#       self.daysList[day.day.day] = day #just maps the day (1-31) to that Day object
 
-   def addMonth(self, month: Month):
-      self.daysList[month.month] = month
+#    def display(self):
+#       #display the month visually
+#       pass
 
-   def display(self):
-      #display the year visually
-      pass
+# class Year:
+#    def __init__(self, year):
+#       self.year = year
+#       self.monthsList = [None]
+#       for i in range(1, 13):
+#          temp = Month(i, year)
+#          self.monthsList.append(temp)
 
-   
+#    def display(self):
+#       #display the year visually
+#       pass
+
    
 
