1121bfc5b5b587e0ee1b25a6fafdb82b5594e801
echavemann <echavemann@gmail.com>
2022-05-14 00:30:30 -0500

    testrebuilds

---
+++ backend/calendar_test.py
@@ -0,0 +1,15 @@
+import calendar
+import pytest
+import datetime
+
+
+
+def test_init():
+    cal = calendar.Calendar()
+    assert (cal.taskList) == {}
+    assert cal.calendar != {}
+    assert cal.calendar[2022] != {}
+    assert cal.calendar[2022][1] != {}
+    assert cal.calendar[2022][1][datetime.datetime(2022,1,1)] == []
+
+
