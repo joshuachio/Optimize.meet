b95f2788000045e3bbe48b9d0febeafdf0bf9478
joshuachio <jchio@gatech.edu>
2022-05-14 13:31:55 -0500

    Add validation for phone, email, username, password

--- backend/user.py
+++ backend/user.py
@@ -1,18 +1,73 @@
 import datetime
 from calendar import Calendar
 from pydoc import ispackage
+from re import U
+from tkinter import E
 from events import Event
 from tasks import Task
 
 class User:
-    def __init__(self, userID, username, email, password, calendar: Calendar, phoneNumber = None):
+    def __init__(self, userID, username: str, email: str, password: str, calendar: Calendar, phoneNumber = None):
         self.userID = userID
-        self.username = username
-        self.password = password
-        self.email = email
+        self.setUsername(username)
+        self.setPassword(password)
+        self.setEmail(email)
         self.calendar = calendar
-        self.phoneNumber = phoneNumber
+        self.setPhoneNumber(phoneNumber)
         self.friendsList = {}
+
+    #Check for valid username (MUST BE 8-30 characters long, only alphanumeric characters)
+    def setUsername(self, username):
+        if not (7 > len(username) > 30):
+            self.username = None
+            return
+        for char in username:
+            if not (48 <= ord(char) <= 57) or not (65 <= ord(char) <= 90) or not (97 <= ord(char) <= 122):
+                self.username = None
+                return
+        self.username = username
+
+    #Check for valid password (Can change the parameters for valid passwords)
+    def setPassword(self, password):
+        if not (7 > len(password) > 30):
+            self.password = None
+            return
+        for char in password:
+            if not (48 <= ord(char) <= 57) or not (65 <= ord(char) <= 90) or not (97 <= ord(char) <= 122):
+                self.password = None
+                return
+        self.password = password
+
+    #Checks valid email
+    def setEmail(self, email):
+        try:
+            check = email.split("@")
+            if "." in check[1]:
+                self.email = email
+            else:
+                self.email = None
+        except:
+            self.email = None
+
+    #Check for valid phone number
+    def setPhoneNumber(self, number):
+        if number.isdigit():
+            self.phoneNumber = number
+            return
+        count = 0
+        if "+" in number:
+            for char in number:
+                if char.isdigit():
+                    count += 1
+            if count == 11:
+                self.phoneNumber = number
+                return
+        else:
+            for char in number:
+                if char.isdigit():
+                    count += 1
+            if count == 10:
+                self.phoneNumber = number
 
     def createEvent(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime,
                     event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
