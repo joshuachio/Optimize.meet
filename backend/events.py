a28d271bd875ab8028df87e8c9b0e56cd4f2c898
joshuachio <jchio@gatech.edu>
2022-05-13 19:08:30 -0500

    added location attributes

--- backend/events.py
+++ backend/events.py
@@ -5,13 +5,14 @@ repeat = {
    2: datetime.timedelta(weeks=1)
 }
 
-
 class Event:
    def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime,
-                event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
-      self.startDateTime = startDateTime
-      self.endDateTime = endDateTime
+                event_name: str, location, description: str = None, recurring: int = None, isPrivate: bool = True):
+      if startDateTime.date is endDateTime.date:
+         self.setStartDatetime(startDateTime)
+         self.setEndDatetime(endDateTime)
       self.event_name = event_name
+      self.location = location
       self.description = description
       self.recurring = repeat[recurring]
       self.isprivate = isPrivate
@@ -26,6 +27,9 @@ class Event:
    def setEventName(self, name):
       self.event_name = name
 
+   def setLocation(self, location):
+      self.location = location
+
    def setRecurring(self, recurring):
       self.recurring = repeat[recurring]
 
@@ -34,17 +38,29 @@ class Event:
 
 class Course(Event):
 
-   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
-      super().__init__(startDateTime, endDateTime, event_name, description, recurring, isPrivate)
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, location, description: str = None, recurring: int = None, isPrivate: bool = True):
+       super().__init__(startDateTime, endDateTime, event_name, location, description, recurring, isPrivate)
 
 class Meeting(Event):
 
-   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str,
-                description: str = None, recurring: int = None, isPrivate: bool = True, meetingLink: str = None):
-      super().__init__(startDateTime, endDateTime, event_name, description, recurring, isPrivate)
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, location,
+                description: str = None, recurring: int = None, isPrivate: bool = True, meetingLink: str = None, collaboratorList = None):
+      super().__init__(startDateTime, endDateTime, event_name, location, description, recurring, isPrivate)
       self.meetingLink = meetingLink
+      self.collaboratorList = collaboratorList
+
+   def notifyParticipants(self, collaborator):
+      participantList = [collaborator]
+      for c in self.collaboratorList:
+         participantList.append(c)
+      return participantList
 
 class Personal(Event):
 
-   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
-      super().__init__(startDateTime, endDateTime, event_name, description, recurring, isPrivate)
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, location, description: str = None, recurring: int = None, isPrivate: bool = True):
+       super().__init__(startDateTime, endDateTime, event_name, location, description, recurring, isPrivate)
+
+class MultiDay(Event):
+
+   def __init__(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime, event_name: str, location, description: str = None, recurring: int = None, isPrivate: bool = True):
+       super().__init__(startDateTime, endDateTime, event_name, location, description, recurring, isPrivate)
