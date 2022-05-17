6333d425eeec57c32978c3496a62a625a2285a86
joshuachio <jchio@gatech.edu>
2022-05-17 10:36:04 -0500

    Fixed a test

--- backend/calendar_test.py
+++ backend/calendar_test.py
@@ -11,7 +11,7 @@ def test_init():
     assert cal.myCal[2022] != {}
     assert cal.myCal[2022][1] != {}
     assert cal.myCal[2022][1][datetime.datetime(2022,1,1)] == []
-    assert cal.displayTaskList(datetime.datetime(2022,1,1)) == []
+    assert cal.displayTaskList(datetime.datetime(2022,1,1)) == None
     assert cal.displayDay(datetime.datetime(2022,1,1)) == []
     assert cal.showAvailibility(datetime.datetime(2022,1,1)) == []
     assert cal.dailyDigest == []
