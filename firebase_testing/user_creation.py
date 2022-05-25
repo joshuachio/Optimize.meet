9542e1afde524c13c4bdbb5e9d176fda0d9c81a1
joshuachio <jchio@gatech.edu>
2022-05-25 18:56:04 -0500

    added logIn class and changed storing users in byte format

--- firebase_testing/user_creation.py
+++ firebase_testing/user_creation.py
@@ -1,23 +1,27 @@
 import datetime
+import pickle
+
 import jsonpickle
 from collections import OrderedDict
 from Pyrebase4 import pyrebase
 from be_calendar import Calendar
 from be_events import Event
 from be_tasks import Task
+from be_recurring import Recurring
 from config_key import config_values
 
+
 class User:
-    def __init__(self, username: str, email: str, password: str, calendar: Calendar, phoneNumber = None):
-        #This will create a user in the admin dashboard on firebase (ALSO checks for valid email)
+    def __init__(self, username: str, email: str, password: str, calendar: Calendar, phoneNumber=None):
+        # This will create a user in the admin dashboard on firebase (ALSO checks for valid email)
         config = config_values
         firebase = pyrebase.initialize_app(config)
-        auth = firebase.auth()
-        db = firebase.database()
-        storage = firebase.storage()
-        userInstance = auth.create_user_with_email_and_password(email, password)
+        self.auth = firebase.auth()
+        self.db = firebase.database()
+        self.storage = firebase.storage()
+        userInstance = self.auth.create_user_with_email_and_password(email, password)
 
-        #User Class Attributes
+        # User Class Attributes
         self.userID = userInstance["localId"]
         self.setUsername(username)
         self.setPassword(password)
@@ -26,17 +30,7 @@ class User:
         self.phoneNumber = phoneNumber
         self.friendsList = {}
 
-        #Adds the user to firebase storage
-        encodedUser = jsonpickle.encode(self)
-        openFile = open('upload.txt', 'w')
-        print(encodedUser, file=openFile)
-        openFile.close()
-        storage.child("users/" + self.userID + ".txt").put('upload.txt')
-        # dictOfUser = json.loads(encodedUser)
-        # modifiedDictOfUser = OrderedDict(self.modify_dict(dictOfUser))
-        # db.child("users").child(self.userID).set(modifiedDictOfUser)
-
-    #Check for valid username (MUST BE 8-30 characters long, only alphanumeric characters)
+    # Check for valid username (MUST BE 8-30 characters long, only alphanumeric characters)
     def setUsername(self, username):
         if not (7 > len(username) > 30):
             self.username = None
@@ -47,7 +41,7 @@ class User:
                 return
         self.username = username
 
-    #Check for valid password (Can change the parameters for valid passwords)
+    # Check for valid password (Can change the parameters for valid passwords)
     def setPassword(self, password):
         if not (7 > len(password) > 30):
             self.password = None
@@ -58,7 +52,7 @@ class User:
                 return
         self.password = password
 
-    #Check for valid phone number
+    # Check for valid phone number
     def setPhoneNumber(self, number):
         if number.isdigit():
             self.phoneNumber = number
@@ -78,33 +72,18 @@ class User:
             if count == 10:
                 self.phoneNumber = number
 
-    def createEvent(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime,
-                    event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
-        newEvent = Event(startDateTime, endDateTime, event_name, description, recurring, isPrivate)
-        self.calendar.addEvent(newEvent)
+    # Adds the user to firebase storage
+    def addToStorage(self):
 
-    def deleteEvent(self, event):
-        self.calendar.deleteEvent(event)
-
-    def addTask(self, task: Task):
-        self.calendar.addTask(task)
-
-    def addFriend(self, friend):
-        self.friendsList[friend.userID] = friend
-
-    def removeFriend(self, friend):
-        del self.friendsList[friend.userID]
-
-    def showFriendList(self):
-        for friend in self.friendsList.values():
-            #display friend list
-            pass
-
-    def showAvailability(self, startDatetime, endDatetime, showPrivate: bool = False):
-        return self.calendar.showAvailability(startDatetime, endDatetime, showPrivate)
-
-    def showDailyDigest(self, showEvents = True, showTasks = True):
-        return self.calendar.dailyDigest(showEvents, showTasks)
+        openFile = open('upload.bin', 'wb')
+        encodedUser = pickle.dump(self, openFile)
+        # encodedUser = encodedUser.decode('utf-8')
+        # print(encodedUser, file=openFile)
+        openFile.close()
+        self.storage.child("users/" + self.userID + ".bin").put('upload.bin')
+        # dictOfUser = json.loads(encodedUser)
+        # modifiedDictOfUser = OrderedDict(self.modify_dict(dictOfUser))
+        # db.child("users").child(self.userID).set(modifiedDictOfUser)
 
     def modify_dict(self, d):
         new = OrderedDict()
@@ -138,71 +117,87 @@ class User:
                 new[k] = v
         return OrderedDict(new)
 
+
 class Session:
     def __init__(self, email, password):
-        config = {
-            "apiKey": "AIzaSyAz5bLpUWAOBouA8Q9_WeloGYDdI1Q9s5g",
-            "authDomain": "calendar-be058.firebaseapp.com",
-            "databaseURL": "https://calendar-be058-default-rtdb.firebaseio.com",
-            "projectId": "calendar-be058",
-            "storageBucket": "calendar-be058.appspot.com",
-            "messagingSenderId": "765432044481",
-            "appId": "1:765432044481:web:7dceaf5af57e8374572d40",
-            "measurementId": "G-9Q457P91BV"
-        }
+        config = config_values
         firebase = pyrebase.initialize_app(config)
         auth = firebase.auth()
         db = firebase.database()
         storage = firebase.storage()
         userInstance = auth.sign_in_with_email_and_password(email, password)
-        userID = userInstance['localID']
-        storage.child("users/" + userID + ".txt").download('download.txt', 'download.txt')
-        openFile = open('download.txt', 'r')
-        self.user = jsonpickle.decode(openFile.read())
+        userID = userInstance['localId']
+        storage.child("users/" + userID + ".bin").download('download.bin', 'download.bin')
+        openFile = open('download.bin', 'rb')
+        self.user = pickle.loads(openFile.read())
         openFile.close()
 
-    def createUser(self, user: User):
-        pass
+    def createEvent(self, startDateTime: datetime.datetime, endDateTime: datetime.datetime,
+                    event_name: str, description: str = None, recurring: int = None, isPrivate: bool = True):
+        newEvent = Event(startDateTime, endDateTime, event_name, description, recurring, isPrivate)
+        self.calendar.addEvent(newEvent)
+
+    def deleteEvent(self, event):
+        self.calendar.deleteEvent(event)
+
+    def addTask(self, task: Task):
+        self.calendar.addTask(task)
+
+    def addFriend(self, friend):
+        self.friendsList[friend.userID] = friend
+
+    def removeFriend(self, friend):
+        del self.friendsList[friend.userID]
+
+    def showFriendList(self):
+        for friend in self.friendsList.values():
+            # display friend list
+            pass
+
+    def showAvailability(self, startDatetime, endDatetime, showPrivate: bool = False):
+        return self.calendar.showAvailability(startDatetime, endDatetime, showPrivate)
+
+    def showDailyDigest(self, showEvents=True, showTasks=True):
+        return self.calendar.dailyDigest(showEvents, showTasks)
+
+    def logOut(self):
+        self.user.addToStorage()
 
 
-config = {
-            "apiKey": "AIzaSyAz5bLpUWAOBouA8Q9_WeloGYDdI1Q9s5g",
-            "authDomain": "calendar-be058.firebaseapp.com",
-            "databaseURL": "https://calendar-be058-default-rtdb.firebaseio.com",
-            "projectId": "calendar-be058",
-            "storageBucket": "calendar-be058.appspot.com",
-            "messagingSenderId": "765432044481",
-            "appId": "1:765432044481:web:7dceaf5af57e8374572d40",
-            "measurementId": "G-9Q457P91BV"
-        }
-firebase = pyrebase.initialize_app(config)
-# auth = firebase.auth()
-# db = firebase.database()
-# userInstance = auth.sign_in_with_email_and_password("joshuachio10@gmail.com", "JoshuaChio1020")
-# userID = userInstance['localId']
-# user = db.child("users").child(userID).get().val()
-#
-# def unmodify_dict(d):
-#     new = {}
-#     for k, v in d.items():
-#         if type(v) == dict:
-#             v = unmodify_dict(v)
-#         elif type(v) == list:
-#             for i in range(len(v)):
-#                 if type(v[i]) == dict:
-#                     v[i] = unmodify_dict(v[i])
-#         if "XX" in k:
-#             temp = k.replace("XX", "/")
-#             new[temp] = v
-#         else:
-#             new[k] = v
-#     return new
-#
-# print(unmodify_dict(user))
-# print(jsonpickle.decode(str(user)))
+#messy testing stuff disregard
 
+cal = Calendar()
+cal.taskList == {}
+cal.myCal != {}
+cal.taskList == {}
+cal.myCal[2022] != {}
+cal.myCal[2022][1] != {}
+cal.myCal[2022][1][1] == []
 
+# Ensures that an event can be added properly.
+firstTime = datetime.datetime(2022, 1, 1, 9)
+sTime = datetime.datetime(2022, 1, 1, 11)
+testEvent = Event(firstTime, sTime, "First Event", "1927 Orrington Avenue", "Video Call")
+cal.addEvent(testEvent)
+cal.myCal[2022][1][1][0] == testEvent
+cal.myCal[2022][1][1][0].description == "Video Call"
+cal.displayDay(datetime.datetime(2022, 1, 1)) == [testEvent]
+cal.showAvailibility(firstTime, datetime.datetime(2022, 1, 3)) == [testEvent]
 
+secondTime = datetime.datetime(2022, 1, 2, 13)
+tTime = datetime.datetime(2022, 1, 2, 15)
+recurrance = Recurring("DAILY", 2, datetime.datetime(2022, 2, 1))
+secEvent = Event(secondTime, tTime, "Nap Time", "My bed", "Sleeping", recurrance)
+cal.addEvent(secEvent)
+cal.myCal[2022][1][8][0] == secEvent
+cal.myCal[2022][1][2][0] == secEvent
+cal.myCal[2022][1][30][0] == secEvent
+# print(cal.myCal[2022])
 
+email = "joshuachio10@gmail.com"
+password = "JoshuaChio1020"
 
-
+# u = User("Joshua Chio", email, password, cal, 9725911128)
+# u.addToStorage()
+f = Session('joshuachio10@gmail.com', 'JoshuaChio1020')
+print(f.user.calendar.showAvailibility(firstTime, datetime.datetime(2022, 1, 20)))
