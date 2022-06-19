facbc25ff2d8eb77155cf9a14190ac446da480f0
joshuachio <jchio@gatech.edu>
2022-06-19 15:46:02 -0500

    Flask app

---
+++ frontend/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, redirect, url_for, request
+
+app = Flask(__name__)
+
+@app.route("/calendar")
+def calendar():
+    return render_template("calendar.html")
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid'
+        else:
+            return redirect(url_for('calendar'))
+    return render_template('login.html', error=error)
