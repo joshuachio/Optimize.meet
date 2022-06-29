83bc4879e016ff3b1b7648c08dddd7af6185d0a8
Joshua Chio <jchio@gatech.edu>
2022-06-29 11:22:54 -0500

    Changed logout/login viewing?

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,11 +1,7 @@
 from tracemalloc import start
 from flask import Flask, render_template, redirect, url_for, request, session, g
 import asyncio
-import os
-import json
 from datetime import datetime
-# import sys
-# sys.path.append('../Calendar')
 from ..Source.be_calendar import Calendar
 from ..Source.be_events import Event
 from ..Source.be_recurring import Recurring
@@ -24,16 +20,17 @@ def before_request():
     if 'user_id' in session:
         user = Session(session['user_id'][0], session['user_id'][1])
         g.user = user
+        
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', logged_in=False)
 
 @app.route('/home', methods=["GET"])
 def home():
     if not g.user:
         return redirect(url_for('login'))
-    return render_template('homepage.html')
+    return render_template('homepage.html', logged_in=True)
 
 @app.route("/event", methods=['POST', 'GET'])
 def event():
@@ -61,7 +58,7 @@ def event():
         print("success")
 
         return redirect(url_for('home'))
-    return render_template("event.html")
+    return render_template("event.html", logged_in=True)
     
 @app.route("/task", methods=['POST', 'GET'])
 def task():
@@ -79,12 +76,11 @@ def task():
         print("success")
 
         return redirect(url_for('home'))
-    return render_template("task.html")
+    return render_template("task.html", logged_in=True)
 
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-    error = None
     if request.method == 'POST':
         session.pop('user_id', None)
         # Tries to create a login session with the username and password provided
@@ -101,8 +97,13 @@ def login():
         except:
             errorstate = 1
             error = "Invalid username or password"
-            return render_template('login.html', error=error)
-    return render_template('login.html', error=error)
+            return render_template('login.html', logged_in=False)
+    return render_template('login.html', logged_in=False)
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -127,6 +128,6 @@ def register():
         except:
             #Some error occurred, return to register page again
             error = 'Invalid Credentials'
-            return render_template('register.html', error=error)
+            return render_template('register.html', logged_in=False)
 
-    return render_template('register.html')
+    return render_template('register.html', logged_in=False)
